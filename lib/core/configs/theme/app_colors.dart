a1ede26f291633cfb9db41209e5778cc8b659d51
Grimmmm0 <boneclinkz35@gmail.com>
2024-11-26 20:30:12 +0800

    Theme of Application Done

--- lib/core/configs/theme/app_colors.dart
+++ lib/core/configs/theme/app_colors.dart
@@ -4,5 +4,6 @@ class AppColors {
   static const primary = Color(0xffFA812F);
   static const lightBackground = Color(0xffFEF3E2);
   static const darkBackground = Color(0xff0D0C0C);
-  static const grey = Color(0xffB6A28E);
+  static const grey = Color(0xffBEBEBE);
+  static const darkGrey = Color(0xff343434);
 }
