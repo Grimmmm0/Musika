e458565da3946c4d75befccca05f92fec3fae559
Grimmmm0 <boneclinkz35@gmail.com>
2024-11-26 18:52:21 +0800

    Initial Commit, Project Structure Done, Working on Themes

---
+++ lib/core/configs/theme/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const primary = Color(0xffFA812F);
+  static const lightBackground = Color(0xffFEF3E2);
+  static const darkBackground = Color(0xff0D0C0C);
+  static const grey = Color(0xffB6A28E);
+}
