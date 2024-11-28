aae2596307e751cfedad877dec4845656e60caaf
Grimmmm0 <boneclinkz35@gmail.com>
2024-11-29 01:07:37 +0800

    Experimental Splash Screen Added

--- lib/core/configs/theme/app_colors.dart
+++ lib/core/configs/theme/app_colors.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 class AppColors {
   static const primary = Color(0xffFA812F);
-  static const lightBackground = Color(0xffFEF3E2);
+  static const lightBackground = Color(0xffffffff);
   static const darkBackground = Color(0xff0D0C0C);
   static const grey = Color(0xffBEBEBE);
   static const darkGrey = Color(0xff343434);
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:musika/core/configs/theme/app_theme.dart';
+import 'package:musika/presentation/splash/pages/splash.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,6 +12,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo', theme: AppTheme.lightTheme, home: Container());
+        debugShowCheckedModeBanner: false,
+        title: 'Musika',
+        theme: AppTheme.lightTheme,
+        home: const SplashPage());
   }
 }
