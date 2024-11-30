1dabd7c3db58d89e17c79ea5bf22ec92ab095032
Grimmmm0 <boneclinkz35@gmail.com>
2024-11-30 21:52:22 +0800

    Get Started Page Created and Basic App Button Widget

--- lib/core/configs/assets/app_images.dart
+++ lib/core/configs/assets/app_images.dart
@@ -1,4 +1,10 @@
 class AppImages {
-  static const String basePath = 'assets/vectors/';
+  static const String basePath = 'assets/images/';
   static const String introBG = '${basePath}intro_bg.png';
 }
+
+class AppLogo {
+  static const String basePath = 'assets/images/';
+  static const String introLogo = '${basePath}logo_musika.png';
+}
+
--- lib/presentation/introduction/pages/get_started.dart
+++ lib/presentation/introduction/pages/get_started.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:musika/common/widgets/button/basic_app_button.dart';
+import 'package:musika/core/configs/assets/app_images.dart';
+import 'package:musika/core/configs/theme/app_colors.dart';
 
 class GetStartedPage extends StatelessWidget {
   const GetStartedPage({super.key});
@@ -7,7 +10,61 @@ class GetStartedPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
-        children: [],
+        children: [
+          Container(
+            padding: const EdgeInsets.symmetric(
+              vertical: 40,
+              horizontal: 40
+            ),
+            decoration: const BoxDecoration(
+                image: DecorationImage(
+                    fit: BoxFit.fill, 
+                    image: AssetImage(AppImages.introBG)
+                    )
+                    ),
+
+                    child: Column(
+                      children: [
+                       const Align(
+                        alignment: Alignment.topCenter,
+                         child: Image(image: 
+                         AssetImage(AppLogo.introLogo),
+                          width: 150, height: 250),
+                       ),
+
+                        const Spacer(),
+                        const Text(
+                          'Enjoy listening to Musika',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white,
+                            fontSize: 20,
+                          ),
+                        ),
+
+                        const SizedBox(height: 21),
+
+                          const Text(
+                          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed elementum libero eu lacus imperdiet, in ornare orci imperdiet. In vel nibh aliquam, ornare nibh non, hendrerit neque.',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            color: AppColors.grey,
+                            fontSize: 13,
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                        const SizedBox(height: 20,),
+                        BasicAppButton(onPressed: () {
+                          
+                        }, 
+                        title: 'Get Started')
+                      ],
+                    ),
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.20),
+          )
+        ],
       ),
     );
   }
