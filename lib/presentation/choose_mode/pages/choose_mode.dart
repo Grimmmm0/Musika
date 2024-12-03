2220b2a72295166aedc6eea10006dd430170ac88
Grimmmm0 <boneclinkz35@gmail.com>
2024-12-04 00:43:51 +0800

    Theme Logic,design and Implemantation Done

--- lib/presentation/choose_mode/pages/choose_mode.dart
+++ lib/presentation/choose_mode/pages/choose_mode.dart
@@ -1,8 +1,11 @@
+
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:musika/core/configs/assets/app_vectors.dart';
+import 'package:musika/presentation/choose_mode/bloc/theme_cubit.dart';
 
 import '../../../common/widgets/button/basic_app_button.dart';
 import '../../../core/configs/assets/app_images.dart';
@@ -61,41 +64,77 @@ class ChooseModePage extends StatelessWidget {
                       Row(
                         mainAxisAlignment: MainAxisAlignment.center,
                         children: [
-                          ClipOval(
-                            child: BackdropFilter(
-                              filter: ImageFilter.blur(sigmaX: 10,sigmaY: 10),
-                              child: Container(
-                                height: 80,
-                                width: 80,
-                                decoration: BoxDecoration(
-                                  color: Color(0xff30393C).withOpacity(0.5),
-                                  shape: BoxShape.circle
+                          Column(
+                            children: [
+                              GestureDetector(
+                                 onTap: (){
+                                context.read<ThemeCubit>().updateTheme(ThemeMode.dark);
+                              },
+                                child: ClipOval(
+                                  child: BackdropFilter(
+                                    filter: ImageFilter.blur(sigmaX: 10,sigmaY: 10),
+                                    child: Container(
+                                      height: 80,
+                                      width: 80,
+                                      decoration: BoxDecoration(
+                                        color: const Color(0xff30393C).withOpacity(0.5),
+                                        shape: BoxShape.circle
+                                      ),
+                                      child: SvgPicture.asset(
+                                        AppVectors.moonLogo,
+                                         fit: BoxFit.none,
+                                      ),   
+                                    ),
+                                  ),
                                 ),
-                                child: SvgPicture.asset(
-                                  AppVectors.moonLogo,
-                                   fit: BoxFit.none,
-                                ),   
                               ),
-                            ),
+                              const SizedBox(height: 15,),
+                               const Text('Dark Mode',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 17,
+                            color: AppColors.grey
                           ),
-                          SizedBox(width: 40,),
-                         ClipOval(
-                            child: BackdropFilter(
-                              filter: ImageFilter.blur(sigmaX: 10,sigmaY: 10),
-                              child: Container(
-                                height: 80,
-                                width: 80,
-                                decoration: BoxDecoration(
-                                  color: AppColors.primary,
-                                  shape: BoxShape.circle,
+                          ),
+
+                            ],
+                          ),
+                          const SizedBox(width: 40,),
+
+                         Column(
+                           children: [
+                            GestureDetector(
+                                onTap: (){
+                            context.read<ThemeCubit>().updateTheme(ThemeMode.light);
+                          },
+                               child: ClipOval(
+                                  child: BackdropFilter(
+                                    filter: ImageFilter.blur(sigmaX: 10,sigmaY: 10),
+                                    child: Container(
+                                      height: 80,
+                                      width: 80,
+                                      decoration: const BoxDecoration(
+                                        color: AppColors.primary,
+                                        shape: BoxShape.circle,
+                                      ),
+                                       child: SvgPicture.asset(
+                                        AppVectors.sunLogo,
+                                        fit: BoxFit.none,
+                                      )                     
+                                    ),
+                                  ),
                                 ),
-                                 child: SvgPicture.asset(
-                                  AppVectors.sunLogo,
-                                  fit: BoxFit.none,
-                                )                     
-                              ),
-                            ),
+                             ),
+                              const SizedBox(height: 15,),
+                               const Text('Light Mode',
+                          style: TextStyle(
+                            fontWeight: FontWeight.w500,
+                            fontSize: 17,
+                            color: AppColors.grey
                           ),
+                          ),
+                           ],
+                         ),
                         ],
                       ),
                     
@@ -109,7 +148,7 @@ class ChooseModePage extends StatelessWidget {
                                 )
                                 );
                           }, 
-                          title: 'Get Started')
+                          title: 'Continue')
                         ],
                       ),
           ),
