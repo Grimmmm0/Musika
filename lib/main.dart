b75f428a90003d89551a07afb1e86463fca774b7
Grimmmm0 <boneclinkz35@gmail.com>
2024-12-08 22:29:04 +0800

    Firebase Setup and config Done

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hydrated_bloc/hydrated_bloc.dart';
 import 'package:musika/core/configs/theme/app_theme.dart';
+import 'package:musika/firebase_options.dart';
 import 'package:musika/presentation/choose_mode/bloc/theme_cubit.dart';
 import 'package:musika/presentation/splash/pages/splash.dart';
 import 'package:path_provider/path_provider.dart';
@@ -13,6 +15,9 @@ Future<void> main() async {
     storageDirectory: kIsWeb
         ? HydratedStorage.webStorageDirectory
         : await getApplicationDocumentsDirectory(),
+  );
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform
   );
   runApp(const MyApp());
 }
